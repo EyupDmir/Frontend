4cd065bd6a30d15b9f8a2dd7a939b483ec147d50
Cubetron Software <Cubetron Software@LAPTOP-7NOG9R23>
2022-08-18 16:04:29 +0300

    4.Bölüm Sonu (Id,Stok Adedi Gelmiyor)

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { Category } from 'src/app/models/category';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -17,7 +17,10 @@ export class ProductComponent implements OnInit {
   dataLoaded = false;
   filterText="";
   
-  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute, private toastrService:ToastrService) { } 
+  constructor(private productService:ProductService, 
+    private activatedRoute:ActivatedRoute, 
+    private toastrService:ToastrService,
+    private cartService:CartService ) {} 
 
   ngOnInit(): void {
     
@@ -47,6 +50,12 @@ export class ProductComponent implements OnInit {
   }
 
   addToCart(product:Product){
-    this.toastrService.success("Sepete eklendi",product.productName)
+    if(product.productId===1){
+      this.toastrService.error("Hata","Bu ürün sepete eklenemez")
+    }
+    else{
+      this.toastrService.success("Sepete eklendi",product.productName)
+      this.cartService.addToCart(product);
+    }   
   }
 }
